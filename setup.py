5d8a8d913f800e969f9fdf93a12da92a7bdfa2bd
Maulik Lakhani <maulik.lakhanix@gmail.com>
2024-08-01 16:17:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
 from setuptools import setup, find_packages
 
-# Read the content of the README file
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Read the README file for the long description
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
-    name="PortScout",
-    version="0.1.0",
-    author="Maulik Lakhani",
-    author_email="maulik@example.com",  # Update this to the author's email address
-    description="A simple port scanning tool",
+    name='PortScout',
+    version='0.1.0',
+    author='Maulik Lakhani',
+    author_email='your.email@example.com',  # Replace with the author's email
+    description='A high performance network port scanner.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/MaulikxLakhani/PortScout",
+    long_description_content_type='text/markdown',
+    url='https://github.com/MaulikxLakhani/PortScout',
     packages=find_packages(),
-    py_modules=["PortScout"],
+    include_package_data=True,
+    install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    python_requires='>=3.6',
-    install_requires=[
-    ],
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'portscout=PortScout:main',
