f8a9097b8c4c53634891b201d3cc6f5f1e2f6880
Maulik Lakhani <maulik.lakhanix@gmail.com>
2024-08-02 12:27:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open('README.md') as f:
 
 setup(
     name='PortScout',
-    version='1.0.1',
+    version='1.0.2',
     author='Maulik Lakhani',
-    author_email='maulik.lakhanix@gmail.com',  # Replace with the author's email
-    description='A fast and efficient network port scanner for common ports. ',
+    author_email='maulik.lakhanix@gmail.com',
+    description='A high-performance and efficient network port scanner for common ports. ',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/MaulikxLakhani/PortScout',
@@ -21,10 +21,10 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
