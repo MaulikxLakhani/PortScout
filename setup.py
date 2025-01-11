81f6c193cb1326e40d0e050ff6dd37c02900d1bc
Maulik Lakhani <maulik.lakhanix@gmail.com>
2025-01-11 21:11:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     python_requires='>=3.7',
     entry_points={
